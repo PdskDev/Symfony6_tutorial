12f447b935a39823a0bc51c50b0138a82fc4b3d6
PdskDev <prestadesk@gmail.com>
2022-07-26 00:21:52 +0200

    First View Twig

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,19 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movies/{genre}', name: 'movies_index', defaults:['genre' => null], methods:['GET', 'HEAD'])]
-    public function index($genre): JsonResponse
+    #[Route('/movies', name: 'movies_index')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your '.$genre.' controller!',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
+       return $this->render('index.html.twig', [
+        'title' => 'Advengers: Endgame'
+       ]);
     }
     
     
