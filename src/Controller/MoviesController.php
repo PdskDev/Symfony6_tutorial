35a7b52b696332d1e85c9b0c9b84bf43e991d804
PdskDev <prestadesk@gmail.com>
2022-07-26 15:21:00 +0200

    Dep. Injection in MovieController - method 1

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,19 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'movies_index')]
-    public function index(): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-        $movies = [
-            "Avengers: Endgame",
-            "James Bond",
-            "La 7e compagnie",
-            "Star War",
-            "Spiderman",
-            "Black Panther"
+        $movies = $movieRepository->findAll();
 
-        ];
+        dd($movies);
+        
 
-       return $this->render('index.html.twig', array('movies' => $movies));
+       return $this->render('index.html.twig');
     }
     
     
